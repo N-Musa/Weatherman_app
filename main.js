13f95d3e0b107a087f5f572f05f4f63ddc0a26f5
N-Musa <n.musa@gmx.net>
2021-09-15 09:42:59 +0200

    intial main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+async function getForecast(city) {
+
+  const key = '';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+
+  const res = await fetch(url);
+  const weatherData = await res.json();
+  console.log(weatherData);
+  document.querySelector(
+    ".location"
+  ).innerText = `Weather in ${weatherData.name}`;
+  document.querySelector(".celsius").innerText = `${Math.round(
+    weatherData.main.temp
+  )}°C `;
+  //document.querySelector('.icon').innerText = weatherData[2]
+  //document.querySelector('.caption').innerText =
+  document.querySelector(
+    ".wind"
+  ).innerText = `Wind Speed: ${weatherData.wind.speed}`;
+  document.querySelector(".vis").innerText = `Visibility: ${Math.round(
+    weatherData.visibility
+  )}km `;
+  document.querySelector(".feel").innerText = `Feels Like: ${Math.round(
+    weatherData.main.feels_like
+  )}°C `;
+  document.querySelector(
+    ".pressure"
+  ).innerText = `Pressure: ${weatherData.main.pressure}`;
+  document.querySelector(
+    ".humid"
+  ).innerText = `Humidity: ${weatherData.main.humidity}% `;
+
+  const button = document.querySelector(".btn");
+  const input = document.querySelector(".field");
+  button.addEventListener("click", () => getForecast(input.value));
+  input.addEventListener("keypress", (event) => {
+    if (event.keyCode === 13) {
+      getForecast(input.value);
+    }
+  });
+}
+getForecast("Hamburg");
