6b2014822f83487dd95c083e8e2d2119b92fad0e
N-Musa <n.musa@gmx.net>
2021-12-04 21:34:13 +0100

    intial commit :octocat:

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-async function getForecast(city) {
+import './style.css';
 
-  const key = '';
-  const button = document.querySelector(".btn");
-  const input = document.querySelector(".field");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
-  const res = await fetch(url);
-  const weatherData = await res.json();
+const app = document.getElementById('app');
 
-  document.querySelector( ".location").innerText = `Weather in ${weatherData.name}`;
-  document.querySelector(".celsius").innerText = `${Math.round(weatherData.main.temp)}°C `;
-  document.querySelector('.caption').innerText = `${weatherData.weather[0].description}`
-  document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
-  document.querySelector( ".wind").innerText = `Wind Speed: ${weatherData.wind.speed}`;
-  document.querySelector(".vis").innerText = `Visibility: ${Math.round( weatherData.visibility)}km `;
-  document.querySelector(".feel").innerText = `Feels Like: ${Math.round(weatherData.main.feels_like)}°C `;
-  document.querySelector( ".pressure").innerText = `Pressure: ${weatherData.main.pressure}`;
-  document.querySelector(".humid").innerText = `Humidity: ${weatherData.main.humidity}% `;
-  
-  button.addEventListener("click", () => getForecast(input.value));
-  input.addEventListener("keypress", (event) => {
-    if (event.keyCode === 13) {
-      getForecast(input.value);
-    }
-  });
+let weatherData = [];
 
+const loadForecast = (async (city) => {
+  try {
+      const endpoint = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_API_TOKEN}`);
+      weatherData = await endpoint.json();
+      displayForecast(weatherData);
+  } catch (err) {
+    // eslint-disable-next-line
+    console.error(err);
+  }
+})('berlin')
+
+const displayForecast = (forecast) => {
+  const htmlString = 
+     /*    ` 
+        <div class='mt-2'>
+        
+        <h1 class=''>City: ${forecast.name}</h1>
+        <h2>Temp: ${forecast.main.temp}</h2>
+        </div>
+        ` */
+      
+      app.innerHTML = htmlString;
 }
-getForecast("Hamburg");
