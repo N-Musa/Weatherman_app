f373df7460abd6420a9d6f40c9c4c38935bbef34
N-Musa <n.musa@gmx.net>
2021-12-04 23:19:53 +0100

    Modify & add E-Listener on searchbar

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 import './style.css';
 
-const searchBar = document.getElementById('searchBar');
-const app = document.getElementById('app');
-
+const content = document.getElementById('content');
+const searchBar = document.getElementById('searchBar')
 let weatherData = [];
 
-const loadForecast = (async (city) => {
+searchBar.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    loadForecast(searchBar.value);
+  }
+
+})
+
+const loadForecast = async (city) => {
   try {
     const endpoint = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_API_TOKEN}`);
     weatherData = await endpoint.json();
@@ -14,15 +20,14 @@ const loadForecast = (async (city) => {
     // eslint-disable-next-line
     console.error(err);
   }
-})('doha')
+}
+
+loadForecast('Lisabon')
 
 const displayForecast = (forecast) => {
   const htmlString = `
-  <div class="mt-6">
-  <input type="text" id="searchBar" placeholder="Enter City..." class=" outline-black py-2 mb-6 rounded-md appearance-none w-full">
-  <p>Weather in ${forecast.name}</p>
-
- 
+  <div>
+  <p class="text-2xl font-light">Weather in ${forecast.name}</p>
   <h1 class="text-8xl py-2 m-2 font-bold">${Math.round(forecast.main.temp)}&#x2103;</h1>
    <div class="flex items-center space-x-3 py-1 text-lg font-bold">
   <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png"/>
@@ -39,5 +44,5 @@ const displayForecast = (forecast) => {
     <p class="mt-6 font-light text-xs text-blue-700">Powered by Open Weather API</p>
 </div>
         `
-  app.innerHTML = htmlString;
+  content.innerHTML = htmlString;
 }
